428d3ab1ce0d32b7741372d8224f47c2a55d3fd8
GaneshPrasad27 <61357252+GaneshPrasad27@users.noreply.github.com>
2020-05-25 23:01:24 +0530

    some

--- php/book.php
+++ php/book.php
@@ -1,5 +1,5 @@
 <?php
-
+  
 $db_name = "book";
 $mysql_username = "root";
 $mysql_password = "";
@@ -22,10 +22,12 @@ if(isset($_POST['submit'])){
 
 	$sql = "INSERT INTO book(name, email, phone, date, city, time, address) VALUES ('$field1', '$field2', '$field3', '$field4', '$field5', '$field6', '$field7')";
  	if (mysqli_query($conn, $sql)) {
-		echo "Booked Successfully.";
+		echo "<p style='margin-top: 100px; font-family: sans-serif; text-align: center; font-size: 30px;'>Your Appointment Booked Successfully.<br> Our Professionals will contact you shortly...</font> </p> ";
+		header('refresh:3; url=../index.html');
 	 } else {
 		echo "Error: " . $sql . "" . mysqli_error($conn);
 	 }
 	 mysqli_close($conn);
 }
 ?>
+
